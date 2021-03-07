16cf4e67ea07aa56ad02540ac45aecc3cd79c8a9
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-03-07 15:53:29 -0500

    Started creating the object

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+        },
+    
+    {  type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+        },
+
+
+
+
+];
+
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
