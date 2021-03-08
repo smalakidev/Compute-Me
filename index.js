a73e0064ba45b1387878b7c71c7506df2800954d
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-03-08 18:12:05 -0500

    added more questions to the array and called a function at the bottom of the page

--- index.js
+++ index.js
@@ -10,38 +10,65 @@ const questions = [
     { 
         type: "input",
         name: "title",
-        message: "What is the title of your project?"
+        message: "What is the title of your project?",
     },
     {    
         type: "input",
-        name: "Github Username",
-        message: "What is your Github username"
+        name: "Github",
+        message: "What is your Github username",
 
     },
     {
         type: "input",
-        name: "description"
-        message: "Describe your project"
+        name: "description",
+        message: "Describe your project",
     },
     {   
         type: "input",
         name: "installation",
-        message: "What was the installation process of your project?"
+        message: "What was the installation process of your project?",
     },
     {     
-        type: "input"
-
-
+        type: "input",
+        name: "usage",
+        message: "What is your project used for?",
     },
-
-
+    {
+        type: "list",
+        name: "license",
+        message: "What license would you like to use for your application?",
+        choices: ["MIT","Mozilla Public License 2.0","Apache License 2.0","None",]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Would you like other users to contribute? Where there contributors for this app?",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What testing information can you please provide?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your e-mail?",
+    },
+        ]
 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+    inquirer.prompt(questions)
+        .then(function(answers){
+                console.log(answers)
+                generateMarkdown(answers)
+        })
+}
 
 // Function call to initialize app
 init();
