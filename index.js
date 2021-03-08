626bd7ae127aed2c55a0b2ea9942ccde12c6797a
Santosh Malaki <santoshmalaki@Santoshs-MacBook-Pro.local>
2021-03-07 19:50:53 -0500

    added packages

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
